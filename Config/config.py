43eadd442048e999c13c8e9a21a59946e72a1f68
JanKurdila <lektorpythonav@gmail.com>
2024-05-18 08:49:59 +0200

    17) Vloženie špec. obrázku (trojuhlník), pohybuje sa väčšou rýchlosťou a sú zaň + 10 bodov pri zrážke

--- Config/config.py
+++ Config/config.py
@@ -5,14 +5,18 @@ ROZLISENIE = (580, 780)
 POZADIE = pygame.image.load("IMAGES/pozadie.jpg")
 RAKETA = pygame.image.load("IMAGES/raketa.png")
 METEOR = pygame.image.load("IMAGES/meteor1.png")
+SPEC_OBR = pygame.image.load("IMAGES/t.jpg")
 RYCHLOST_PADU_METEORU = 0
 METEOR_INKREMENT = 4
 POCET_METEOROV = 0
+POCET_ITERACII_METEOROV = 0 # aby som generoval pad špec. obrázka napr. po každej druhej iteraci meteorov
 
 SURADNICE_RAKETY = (250, 685)
 # SURADNICE_METEORU = (250, 30) už nepotrebujeme, lebo ich bude generovať funkcia
 
 POZICIA_TEXTU_KONCA_HRY = (175, 400)
+
+RYCHLEJSIA_RYCHLOST_PADU = 4
 
 FPS = 25
 STEP = 5
@@ -20,4 +24,4 @@ STEP = 5
 pygame.init()
 FONT_HRY = pygame.font.SysFont("comicsans", 50)
 FARBA_TEXTU = pygame.Color(255, 255, 255)
-POZICIA_TEXTU = (320, 10)
+POZICIA_TEXTU = (280, 10)
--- main.py
+++ main.py
@@ -9,13 +9,13 @@ def move_rocket(x_coordinate, keys):
         if x_coordinate > 10:  # Aby raketa nešla za ľavý okraj
             x_coordinate -= config.STEP  # Pohyb rakety vľavo
     if keys[pygame.K_RIGHT]:
-        if x_coordinate < 480:  # Aby raketa nešla za pravý okraj
+        if x_coordinate < config.ROZLISENIE[0] - config.RAKETA.get_width() - 10:  # Aby raketa nešla za pravý okraj
             x_coordinate += config.STEP  # Pohyb rakety vpravo
     return x_coordinate
 
 def generate_meteor(meteor_image):
     """Funkcia bude generovať meteor na náhodnom mieste"""
-    return {'x': random.choice(range(10, 440, 50)),
+    return {'x': random.choice(range(10, config.ROZLISENIE[0] - meteor_image.get_width(), 50)),
             'y': random.choice(range(-10, -500, -50)),
             'mask': pygame.mask.from_surface(meteor_image)}
 
@@ -23,9 +23,15 @@ def is_collision(mask1, mask2, mask1_coordinate, mask2_coordinate):
     """Funkcia zistí či nastala kolízia meteoru a rakety"""
     x_off = mask2_coordinate[0] - mask1_coordinate[0]
     y_off = mask2_coordinate[1] - mask1_coordinate[1]
-    if mask1.overlap(mask2, (x_off, y_off)):  # Funkcia overlap zisťuje prekrytie masiek
-        return True
-    return False
+    return mask1.overlap(mask2, (x_off, y_off)) is not None
+
+def generate_special_item(special_image):
+    """Funkcia bude generovať špeciálny objekt na náhodnom mieste"""
+    return {
+        'x': random.choice(range(10, config.ROZLISENIE[0] - special_image.get_width(), 50)),
+        'y': random.choice(range(-10, -500, -50)),
+        'mask': pygame.mask.from_surface(special_image)
+    }
 
 if __name__ == "__main__":
     pygame.init()
@@ -35,6 +41,7 @@ if __name__ == "__main__":
     pozadie = config.POZADIE
     raketa = config.RAKETA
     meteor_image = config.METEOR  # Musíme premenovať meteor na napr. meteor_image, lebo nižšie používame cyklus for meteor in meteory:
+    special_image = config.SPEC_OBR
     font_hry = config.FONT_HRY
 
     # Ideme riešiť kolíziu rakety a meteoru
@@ -44,8 +51,10 @@ if __name__ == "__main__":
 
     score = 0
     meteory = []
+    special_items = []
 
-    koniec_hry = False   #   Ukončenie hry
+    koniec_hry = False   # Ukončenie hry
+    
 
     while True:
         keys = pygame.key.get_pressed()
@@ -56,6 +65,11 @@ if __name__ == "__main__":
             config.POCET_METEOROV += config.METEOR_INKREMENT
             for i in range(config.POCET_METEOROV):
                 meteory.append(generate_meteor(meteor_image))
+            config.POCET_ITERACII_METEOROV += 1
+
+        if config.POCET_ITERACII_METEOROV % 3 == 0:
+            special_items.append(generate_special_item(special_image))
+            config.POCET_ITERACII_METEOROV += 1  # Zabezpečí, že sa špeciálny objekt objaví iba každé dve iterácie
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -75,14 +89,20 @@ if __name__ == "__main__":
                     score += 1
                     meteory.remove(meteor)
                 if is_collision(maska_rakety, meteor['mask'], (x, config.SURADNICE_RAKETY[1]), (meteor['x'], meteor['y'])):
-                    # print("GAME OVER")
-                    # Keď prejde meteor cez raketu tak trba urobiť hneď stop hry, inak sa hra neukonči1!!!
                     koniec_hry = True
+
+            for item in special_items[:]:
+                window.blit(special_image, (item['x'], item['y']))
+                item['y'] += config.RYCHLEJSIA_RYCHLOST_PADU  
+                if item['y'] > config.ROZLISENIE[1]:
+                    special_items.remove(item)
+                if is_collision(maska_rakety, item['mask'], (x, config.SURADNICE_RAKETY[1]), (item['x'], item['y'])):
+                    score += 10
+                    special_items.remove(item)
                 
         if koniec_hry:
             text_na_konci_hry = font_hry.render(f"GAME OVER", True, config.FARBA_TEXTU)
             window.blit(text_na_konci_hry, config.POZICIA_TEXTU_KONCA_HRY)
-
 
         window.blit(score_text, config.POZICIA_TEXTU)
 
